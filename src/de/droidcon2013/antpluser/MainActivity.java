8bf04326471b8987272ac0120a0ea424657d1ef5
shtefan.anca@gmail.com <shtefan.anca@gmail.com@5bd20736-8144-b39e-4a59-a6be01b6c4e9>
2013-04-07 15:38:52 +0000

    Colors

--- src/de/droidcon2013/antpluser/MainActivity.java
+++ src/de/droidcon2013/antpluser/MainActivity.java
@@ -6,6 +6,7 @@ import java.util.Timer;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -18,188 +19,232 @@ import com.dsi.ant.plugins.antplus.pcc.AntPlusHeartRatePcc.IHeartRateDataTimesta
 
 public class MainActivity extends Activity {
 
-	AntPlusHeartRatePcc hrPcc = null;
+    final String TAG = "Pulse";
 
-	TextView tvBpm;
-	TextView tvLastTime;
-	RelativeLayout background;
+    AntPlusHeartRatePcc hrPcc = null;
 
-	Timer timer = new Timer();
+    TextView tvBpm;
+    TextView tvLastTime;
+    RelativeLayout background;
 
-	int computedHeartRate = -1;
-	BigDecimal lastEvent;
-	boolean stillRunning = false;
+    Timer timer = new Timer();
 
-	boolean firstRun = true;
+    int computedHeartRate = -1;
+    BigDecimal lastEvent;
+    boolean stillRunning = false;
 
-	boolean lastColorRed = false;
+    boolean firstRun = true;
 
-	CountDownTimer counter;
+    boolean lastColorRed = false;
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+    CountDownTimer counter;
 
-		background = (RelativeLayout) findViewById(R.id.background);
-		tvBpm = (TextView) findViewById(R.id.bpm);
 
-		tvLastTime = (TextView) findViewById(R.id.lastTime);
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-		if (hrPcc != null) {
-			hrPcc.releaseAccess();
-			hrPcc = null;
-		}
+        background = (RelativeLayout) findViewById(R.id.background);
+        tvBpm = (TextView) findViewById(R.id.bpm);
 
-		counter = new MyCounter(Integer.MAX_VALUE, 2000).start();
+        tvLastTime = (TextView) findViewById(R.id.lastTime);
 
-		AntPlusHeartRatePcc.requestAccess(this, this, false,
-				new IPluginAccessResultReceiver<AntPlusHeartRatePcc>() {
-					@Override
-					public void onResultReceived(AntPlusHeartRatePcc result,
-							int resultCode, int initialDeviceStateCode) {
-						switch (resultCode) {
-						case AntPluginMsgDefines.MSG_REQACC_RESULT_whatSUCCESS:
-							hrPcc = result;
-							subscribeToEvents();
-							break;
-						case AntPluginMsgDefines.MSG_REQACC_RESULT_whatCHANNELNOTAVAILABLE:
-							break;
-						case AntPluginMsgDefines.MSG_REQACC_RESULT_whatOTHERFAILURE:
-							break;
-						case AntPluginMsgDefines.MSG_REQACC_RESULT_whatDEPENDENCYNOTINSTALLED:
-							break;
-						case AntPluginMsgDefines.MSG_REQACC_RESULT_whatUSERCANCELLED:
-							break;
-						default:
-							break;
-						}
-					}
+        if (hrPcc != null) {
+            hrPcc.releaseAccess();
+            hrPcc = null;
+        }
 
-					private void subscribeToEvents() {
+        counter = new MyCounter(Integer.MAX_VALUE, 500).start();
 
-						hrPcc.subscribeHeartRateDataEvent(new IHeartRateDataReceiver() {
-							@Override
-							public void onNewHeartRateData(
-									final int currentMessageCount,
-									final int computedHeartRate,
-									final long heartBeatCounter) {
-								runOnUiThread(new Runnable() {
-									@Override
-									public void run() {
-										// tv_msgsRcvdCount.setText(String.valueOf(currentMessageCount));
-										MainActivity.this.computedHeartRate = computedHeartRate;
-										tvBpm.setText(String
-												.valueOf(computedHeartRate));
-										// tv_heartBeatCounter.setText(String.valueOf(heartBeatCounter));
 
-										/*
-										 * if (MainActivity.this.firstRun) {
-										 * MainActivity.this.timer.schedule(
-										 * changeColorTask,
-										 * MainActivity.this.getHeartRate() );
-										 * MainActivity.this.firstRun = false; }
-										 */
-									}
-								});
-							}
-						});
+        AntPlusHeartRatePcc.requestAccess(this, this, false,
+                new IPluginAccessResultReceiver<AntPlusHeartRatePcc>() {
+                    @Override
+                    public void onResultReceived(AntPlusHeartRatePcc result,
+                            int resultCode, int initialDeviceStateCode) {
+                        switch (resultCode) {
+                        case AntPluginMsgDefines.MSG_REQACC_RESULT_whatSUCCESS:
+                            hrPcc = result;
+                            subscribeToEvents();
+                            break;
+                        case AntPluginMsgDefines.MSG_REQACC_RESULT_whatCHANNELNOTAVAILABLE:
+                            break;
+                        case AntPluginMsgDefines.MSG_REQACC_RESULT_whatOTHERFAILURE:
+                            break;
+                        case AntPluginMsgDefines.MSG_REQACC_RESULT_whatDEPENDENCYNOTINSTALLED:
+                            break;
+                        case AntPluginMsgDefines.MSG_REQACC_RESULT_whatUSERCANCELLED:
+                            break;
+                        default:
+                            break;
+                        }
+                    }
 
-						hrPcc.subscribeHeartRateDataTimestampEvent(new IHeartRateDataTimestampReceiver() {
-							@Override
-							public void onNewHeartRateDataTimestamp(
-									final int currentMessageCount,
-									final BigDecimal timestampOfLastEvent) {
-								runOnUiThread(new Runnable() {
-									@Override
-									public void run() {
-										if (!timestampOfLastEvent
-												.equals(MainActivity.this.lastEvent)) {
-											MainActivity.this.stillRunning = true;
-										} else {
-											MainActivity.this.stillRunning = false;
-										}
-										MainActivity.this.lastEvent = timestampOfLastEvent;
-										tvLastTime.setText(String
-												.valueOf(timestampOfLastEvent));
+                    private void subscribeToEvents() {
 
-										// tv_timestampOfLastEvent.setText(String.valueOf(timestampOfLastEvent));
-									}
-								});
-							}
-						});
+                        hrPcc.subscribeHeartRateDataEvent(new IHeartRateDataReceiver() {
+                            @Override
+                            public void onNewHeartRateData(
+                                    final int currentMessageCount,
+                                    final int computedHeartRate,
+                                    final long heartBeatCounter) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        // tv_msgsRcvdCount.setText(String.valueOf(currentMessageCount));
+                                        MainActivity.this.computedHeartRate = computedHeartRate;
+                                        tvBpm.setText(String
+                                                .valueOf(computedHeartRate));
+                                        // tv_heartBeatCounter.setText(String.valueOf(heartBeatCounter));
+                                        /*
+                                         * if (MainActivity.this.firstRun) {
+                                         * MainActivity.this.timer.schedule(
+                                         * changeColorTask,
+                                         * MainActivity.this.getHeartRate() );
+                                         * MainActivity.this.firstRun = false; }
+                                         */
+                                    }
+                                });
+                            }
+                        });
 
-					}
-				}, new IDeviceStateChangeReceiver() {
-					@Override
-					public void onDeviceStateChange(final int newDeviceState) {
-						runOnUiThread(new Runnable() {
-							@Override
-							public void run() {
-								// tv_status.setText(sdmPcc.getDeviceName() +
-								// ": " +
-								// AntPlusStrideSdmPcc.statusCodeToPrintableString(newDeviceState));
-							}
-						});
-					}
-				});
-	}
+                        hrPcc.subscribeHeartRateDataTimestampEvent(new IHeartRateDataTimestampReceiver() {
+                            @Override
+                            public void onNewHeartRateDataTimestamp(
+                                    final int currentMessageCount,
+                                    final BigDecimal timestampOfLastEvent) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        if (!timestampOfLastEvent
+                                                .equals(MainActivity.this.lastEvent)) {
+                                            MainActivity.this.stillRunning = true;
+                                            if (computedHeartRate > 0 && firstRun ) {
+                                                Log.d( TAG, "Starting counter" );
+//                                                counter = new MyCounter(Integer.MAX_VALUE, getHeartRate()).start();
+                                                firstRun = false;
+                                            }
+                                        } else {
+                                            MainActivity.this.stillRunning = false;
+                                        }
 
-	class MyCounter extends CountDownTimer {
-		int i = 0;
+                                        MainActivity.this.lastEvent = timestampOfLastEvent;
+                                        tvLastTime.setText(String
+                                                .valueOf(timestampOfLastEvent));
 
-		public MyCounter(long millisInFuture, long countDownInterval) {
-			super(millisInFuture, countDownInterval);
+                                        // tv_timestampOfLastEvent.setText(String.valueOf(timestampOfLastEvent));
+                                    }
+                                });
+                            }
+                        });
 
-		}
+                    }
+                }, new IDeviceStateChangeReceiver() {
+                    @Override
+                    public void onDeviceStateChange(final int newDeviceState) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // tv_status.setText(sdmPcc.getDeviceName() +
+                                // ": " +
+                                // AntPlusStrideSdmPcc.statusCodeToPrintableString(newDeviceState));
+                            }
+                        });
+                    }
+                });
+    }
 
-		@Override
-		public void onFinish() {
-			this.start();
-		}
+    class MyCounter extends CountDownTimer {
+        int i = 0;
 
-		@Override
-		public void onTick(long arg0) {
-			i++;
-			background.setBackgroundColor(255001110 * i);
-		}
-	}
+        public MyCounter(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
 
-	protected long getHeartRate() {
-		return 60000 / this.computedHeartRate;
-	}
+        }
 
-	/*
-	 * public TimerTask changeColorTask = new TimerTask() {
-	 * 
-	 * @Override public void run() { MainActivity.this.runOnUiThread( new
-	 * Runnable() {
-	 * 
-	 * @Override public void run() {
-	 * MainActivity.this.background.setBackgroundColor(
-	 * MainActivity.this.getNextColor() ); // TimerTask.this.cancel();
-	 * MainActivity.this.timer.purge(); MainActivity.this.timer.schedule(
-	 * changeColorTask, MainActivity.this.getHeartRate() ); } } ); }
-	 * 
-	 * };
-	 */
+        @Override
+        public void onFinish() {
+            this.start();
+        }
 
-	private int getNextColor() {
-		if (lastColorRed) {
-			lastColorRed = false;
-			return android.R.color.black;
-		} else {
-			lastColorRed = true;
-			return android.R.color.white;
-		}
-	}
+        @Override
+        public void onTick(long arg0) {
+            i++;
+            Log.d( TAG, "Changing color" );
+            background.setBackgroundColor(132 * i + 50);
+            background.invalidate();
+//            background.setBackgroundColor(getNextColor());
+        }
+    }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		if (hrPcc != null) {
-			hrPcc.releaseAccess();
-		}
-	}
+    protected long getHeartRate() {
+        return 60000 / this.computedHeartRate;
+    }
+
+    /*
+     * public TimerTask changeColorTask = new TimerTask() {
+     *
+     * @Override public void run() { MainActivity.this.runOnUiThread( new
+     * Runnable() {
+     *
+     * @Override public void run() {
+     * MainActivity.this.background.setBackgroundColor(
+     * MainActivity.this.getNextColor() ); // TimerTask.this.cancel();
+     * MainActivity.this.timer.purge(); MainActivity.this.timer.schedule(
+     * changeColorTask, MainActivity.this.getHeartRate() ); } } ); }
+     *
+     * };
+     */
+
+
+
+
+//    public class ChangeColorTask extends TimerTask {
+//
+//        Activity act;
+//
+//        public ChangeColorTask( Activity act) {
+//            this.act = act;
+//        }
+//
+//        @Override
+//        public void run() {
+//            MainActivity.this.runOnUiThread( new Runnable() {
+//
+//                @Override
+//                public void run() {
+//                    MainActivity.this.background.setBackgroundResource( MainActivity.this.getNextColor() );
+//                    MainActivity.this.scheduleTask();
+//                }
+//            });
+//        }
+//    }
+
+
+//    private void scheduleTask() {
+//        MainActivity.this.timer = new Timer();
+//        ChangeColorTask newTask = new ChangeColorTask( this );
+//        MainActivity.this.timer.schedule( newTask, MainActivity.this.getHeartRate() );
+//    }
+
+    private int getNextColor() {
+        if (lastColorRed) {
+            lastColorRed = false;
+            return R.color.yellow;
+        } else {
+            lastColorRed = true;
+            return R.color.white;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if ( hrPcc != null ) {
+            hrPcc.releaseAccess();
+        }
+        //sdmPcc.releaseAccess();
+    }
 }
